ffaeaef0c8fe9c875955e58a5d1de3d23a0f507b
lindenzheng <linden@parry8.com>
2026-05-19 13:39:08 -0400

    Animated sun/moon.

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Logo = () => {
     return (
-        <div className="logo-section">
+        <div className="logo">
             <div className="circle"></div>
+            <div class="container">
+                <div class="moon">
+                    <div class="light"></div>
+                    <div class="sphere"></div>
+                </div>
+            </div>
 
             <div className="logo-text">
                 <img src="/assets/text-purple.svg" alt="F!ROSH WEEK" />
